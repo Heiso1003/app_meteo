20df662bfe51064dc4d3fec11644ee6625c83887
Heiso1003 <thomas.debuysscher.13037@stu.he-ferrer.eu>
2021-10-20 08:30:30 +0200

    Premier Js et update Index

--- js/scripts.js
+++ js/scripts.js
@@ -1,128 +1,53 @@
-$( document ).ready(function() {
+$(document).ready(function(){
 
 
-    // console.log( "ready!" );
-    // alert("ready");
+    $('#go').click(function (){
 
+    var city = $('#ville').val();
 
-// $("h1:not(#titre1)").fadeOut(2000);
+    $('h1').html(' Ville : ' +city)
+        $.ajax({
+          //1 definir le fichier à appeler
+          url : 'https://www.prevision-meteo.ch/services/json/'+city,		
+          //2 methode utilisée pour envoyer les variables
 
-// $("h1, p, a, button").click(function(e){
-//     e.preventDefault();
+          success : function(Obj){
+  console.log(Obj)
+            // $('#hier img').attr( 'src', Obj.fcst_day_0.icon_big)
+            // $('#hier span').html(Obj.fcst_day_0.tmax+' ° ')
+            $('#aujourdhui h3').html(Obj.fcst_day_1.day_long)
+            $('#aujourdhui img').attr( 'src', Obj.fcst_day_1.icon_big)
+            $('#aujourdhui span').html('Température max : ' +Obj.fcst_day_1.tmax+' ° ')
+            $('#aujourdhui p').html('Température min : ' +Obj.fcst_day_1.tmin+' ° ')
+            $('#demain h3').html(Obj.fcst_day_2.day_long)
+            $('#demain img').attr( 'src', Obj.fcst_day_2.icon_big)
+            $('#demain span').html('Température max : ' +Obj.fcst_day_2.tmax+' ° ')
+            $('#demain p').html('Température min : ' +Obj.fcst_day_2.tmin+' ° ')
+            $('#apres_demain h3').html(Obj.fcst_day_3.day_long)
+            $('#apres_demain img').attr( 'src', Obj.fcst_day_3.icon_big)
+            $('#apres_demain span').html('Température max : ' +Obj.fcst_day_3.tmax+' ° ')
+            $('#apres_demain p').html('Température min : ' +Obj.fcst_day_3.tmin+' ° ')
+            $('#apres_apres_demain h3').html(Obj.fcst_day_4.day_long)
+            $('#apres_apres_demain img').attr( 'src', Obj.fcst_day_4.icon_big)
+            $('#apres_apres_demain span').html('Température max : ' +Obj.fcst_day_4.tmax+' ° ')
+            $('#apres_apres_demain p').html('Température max : ' +Obj.fcst_day_4.tmin+' ° ')
 
-//     $(this).fadeOut(2000);
+  //  var txt = "";
+      //   for(a = 0; a<Obj.length; a++){
+           // console.log(a)
 
-//     // console.log("click")
+           // txt += '<div class="info"><h2>'+Obj[a].title+'</h2><p>'+Obj[a].body+'</p></div>'
 
-//   }); //click
+      
 
-
-// var monlien = $("#premier").attr("href");
-// $("input").val(monlien);
-
-
-// $("a").click(function(e){
-//     e.preventDefault();
-//     // var lien = $(this).attr("href");
-//     $(".tous, .filles, .garcons ,.nonbinaires").fadeOut(2000);
-//     // $(".filles").fadeOut(2000));
-//     // $(".garcons").fadeOut(2000);
-//     // $(".nonbinaires").fadeOut(2000);
-// })
-
-
-
-$( "li a" ).click(function(e){
-    e.preventDefault();
-    var gender = "."+$(this).attr("data-gender");
-    console.log(gender)
-
-    if(gender == "tous") {
-        $( ".col-md-6" ).fadeIn();
-    } else {
-        $( gender ).fadeIn();
-        $( ".col-md-6:not("+gender+")").fadeOut();
-    }
-});
-
-
-// $("a").click(function(e){
-//     e.preventDefault();
-//     var lien = $(this).attr("href");
-//     $("#titre1").text(lien);
-//     $("input").val(lien);
-// })
-
-
-// var label = $('label[for="hajar"]').text();
-// console.log(label);
-
-
-// $("#go").click(function(e){
-//     e.preventDefault();
-//     console.log("go");
-
-//     var valeur = $("#hajar").val();
-//     console.log(valeur);
-
-//     $("#titre1").text(valeur);
-// })
-
-
-// $("#hajar").keyup+
-// (function(){
-//     var valeur = $(this).val();
-//     $("#titre1").text(valeur);
-// });
-
-
-
-
-// var label = $('label[for="hajar"]').text();
-// console.log(label);
-
-
-// var contenu = $("#moninput").html();
-// console.log(contenu);
-
-
-$(".madiv").append("<h1>coucou</h1>");
-$("#moninput").val("quelque chose")
-
-
-
-
-
-//   $("#deuxieme").click(function(e){
-//     e.preventDefault();
-
-//     $("p").fadeOut(2000);
-
-//     // console.log("click")
-
-//   }); //click
-
-
-
-//   $("#troisieme").click(function(e){
-//     e.preventDefault();
-
-//     $("button").fadeOut(2000);
-
-//     // console.log("click")
-
-//   }); //click
-
-
-
-
-
-
-
-
-
-
-
+       //  $('#contenu').html(txt)
+          } // succes
+        
     
-   
-}); //ready
+    
+    })
+})
+
+
+
+})
