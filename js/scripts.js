ce6dbdc3462c8dfde1a0b43a5a5389b8dd202887
Heiso1003 <thomas.debuysscher.13037@stu.he-ferrer.eu>
2021-10-20 09:17:17 +0200

    Day1 js

--- js/scripts.js
+++ js/scripts.js
@@ -31,16 +31,28 @@ $(document).ready(function(){
             // $('#apres_apres_demain img').attr( 'src', Obj.fcst_day_4.icon_big)
             // $('#apres_apres_demain span').html('Température max : ' +Obj.fcst_day_4.tmax+' ° ')
             // $('#apres_apres_demain p').html('Température max : ' +Obj.fcst_day_4.tmin+' ° ')
+
+
+            // CURRENT DAY
             
         $('#aujourdhui h1').html(city)
-        $('#aujourdhui h2').html(Obj.fcst_day_1.day_long + " " + Obj.current_condition.date)
+        $('#aujourdhui h2').html(Obj.fcst_day_0.day_long + " " + Obj.current_condition.date)
         $('#aujourdhui h3').html(Obj.current_condition.condition)
-        $('#aujourdhui img').attr( 'src', Obj.fcst_day_1.icon_big)
+        $('#aujourdhui img').attr( 'src', Obj.fcst_day_0.icon_big)
         $('#aujourdhui span').html('Température: ' +Obj.current_condition.tmp+'° ')
         $('#aujourdhui p').html('Humidité : ' +Obj.current_condition.humidity+' % ')
         $('#aujourdhui .vent').html('Vitesse : ' +Obj.current_condition.wnd_spd+' km/h  '+ Obj.current_condition.wnd_dir)
         $('#aujourdhui .pression').html('Pression : ' +Obj.current_condition.pressure+' Pa')
 
+
+            // DAY 1
+
+        $('#jour1 h1').html(city)
+        $('#jour1 h2').html(Obj.fcst_day_1.day_long + " " + Obj.fcst_day_1.date)
+        $('#jour1 h3').html(Obj.fcst_day_1.condition)
+        $('#jour1 img').attr( 'src', Obj.fcst_day_1.icon_big)
+        $('#jour1 span').html('Température min: ' +Obj.fcst_day_1.tmin+'° ')
+        $('#jour1 p').html('Température max : ' +Obj.fcst_day_1.tmax+' % ')
 
   //  var txt = "";
       //   for(a = 0; a<Obj.length; a++){
